8885909ce05e901c9c72ad0ffcc07181d1a6910c
SJY <SJY@DESKTOP-IU8KJFO>
2017-05-15 13:47:34 +0900

    실습과제 제출

--- JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -26,19 +26,15 @@ public class SmartPhoneTest {
 				new IPhone("iPhone 7","애플",900000),
 				new Galaxy("갤럭시 S8","삼성",800000)
 		};
-		System.out.println(arr[0].toString());
-		arr[0].turnOn();
-		arr[0].pay();
-		arr[0].useSpecialFunctional(arr[0]);
-		arr[0].turnOff();
-		
-		System.out.println();
-		
-		System.out.println(arr[1].toString());
-		arr[1].turnOn();
-		arr[1].pay();
-		arr[1].useSpecialFunctional(arr[1]);
-		arr[1].turnOff();
+	
+		for(SmartPhone a:arr){
+			System.out.println(a.toString());
+			a.turnOn();
+			a.pay();
+			a.useSpecialFunctional(a);
+			a.turnOff();
+			System.out.println();
+		}
 	}
 
 }
