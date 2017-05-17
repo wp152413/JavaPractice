8446f0f4a77ebc8fc8439088d35d01e2611f2d8d
SJY <SJY@DESKTOP-IU8KJFO>
2017-05-17 10:55:25 +0900

    실습과제 제출

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhone.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *  |_SmartPhone
+ *
+ * 1. 개요 :
+ * 2. 작성일  : 2017. 5. 15.
+ * <pre>
+ *
+ * @author : SJY
+ * @version : 1.0
+ */
+public abstract class SmartPhone {
+	
+	private String model;
+	private String company;
+	private int price;
+	
+	
+	public SmartPhone(){
+		
+	}
+	
+	public SmartPhone(String model,String company,int price){
+		this.model=model;
+		this.company=company;
+		this.price=price;
+	}
+	
+	public void turnOn()
+	{
+		System.out.println(model+"의 전원을 켭니다.");
+	}
+	
+	public void turnOff()
+	{
+		System.out.println(model+"의 전원을 끕니다.");
+	}
+	
+	public abstract void pay();
+	
+	
+	public void useSpecialFunctional(){
+		if(this instanceof IPhone){
+			IPhone b=(IPhone) this;
+			((IPhone)this).useAirDrop();
+		}
+		if(this instanceof Galaxy){
+			Galaxy b=(Galaxy) this;
+			((Galaxy)this).useWirelessCharging();
+		}
+		
+	}
+
+	public String toString() {
+		return "모델명 :" + model + ",제조사 :" + company + ",가격:" + String.format("%,d",price) + "원";
+	}
+	
+	
+}
---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *  |_SmartPhoneTest
+ *
+ * 1. 개요 :
+ * 2. 작성일  : 2017. 5. 15.
+ * <pre>
+ *
+ * @author : SJY
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		SmartPhone [] arr={
+				new IPhone("iPhone 7","애플",900000),
+				new Galaxy("갤럭시 S8","삼성",800000)
+		};
+	
+		for(SmartPhone a:arr){
+			System.out.println(a.toString());
+			a.turnOn();
+			a.pay();
+			a.useSpecialFunctional();
+			a.turnOff();
+			System.out.println();
+		}
+	}
+
+}
