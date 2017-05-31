6c3c328438fd4ac1e5358bd720ba04090d05b9a3
SJY <SJY@DESKTOP-IU8KJFO>
2017-05-31 10:43:54 +0900

    실습과제 제출

---
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * exception
+ *  |_MovieTest
+ *
+ * 1. 개요 :
+ * 2. 작성일  : 2017. 5. 31.
+ * <pre>
+ *
+ * @author : SJY
+ * @version : 1.0
+ */
+public class MovieTest {
+
+	public static void main(String[] args){
+		Movie[] movies={
+				new Movie("에이리언:커버넌트",15),
+				new Movie("가디언즈 오브 갤럭시",12)
+		};
+		int age=13;
+		for(Movie movie:movies){
+			try {
+				movie.buyTicket(age);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+}
